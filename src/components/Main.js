f188b45ec9fdbfdbd8fda03c35db2fc70074f24a
yangsurui <yangtvxq@163.com>
2017-07-10 00:10:09 +0800

    7.9

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,59 +2,68 @@ require('normalize.css/normalize.css');
 require('styles/App.css');
 
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-let songUrlArr = require('../data/songData.json');
-
-(songUrlArr =>{
-  for(let i = 0, j = songUrlArr.length; i < j; i++){
-    let songItem = songUrlArr[i];
-    songItem.songUrl = require('../audios/' + songItem.songName);
-    songUrlArr[i] = songItem;
-  }
-  return songUrlArr;
-
-})(songUrlArr);
-
-
-class SearchBar extends React.Component {
-  render() {
-    return (
-      <div className="search-bar">
-        <input className="search" type="text" placeholder="搜索关键字"/>
-        <span className="searchBtn"></span>
-      </div>
-    )
-  }
-}
+/**
+ * arr[0]: 歌曲索引
+ * arr[1]: 歌曲名
+ * arr[2]: 歌手
+ * arr[3]: 歌曲路径
+ * arr[4][0]: 所属专辑名
+ * arr[4][1]: 所属专辑图片路径
+ */
+let songInfoArr = require('../data/songData.json');
 
 class SongList extends React.Component {
 
-  componentDidMount(){
-
-  }
   render(){
+    let songList = '';
+    for(let i = 0, j= songInfoArr.length; i<j; i++ ){
+      songList += '<li>'+songInfoArr[i][1]+'</li>';
+    }
     return(
       <div className="song-list-container">
-        <ul className="song-list">
-
+        <ul id="song-list">
+          {songList}
         </ul>
       </div>
     )
   }
 }
+class SongInfo extends React.Component {
 
 
-class LyricsList extends React.Component {
-
-}
-
-class Player extends React.Component {
-  render() {
-    let musicSrc = [];
-
+  render(){
     return(
-      <div>
+      <div className="song-info-container">
+        <div className="album-pic">
+          <img src={}/>
+        </div>
+        <div className="song-name"></div>
+        <div className="singer"></div>
+      </div>
+    )
+  }
+}
+class Player extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      //初始化音乐播放器的状态
+      currentSongIndex: 0, //当前播放歌曲的索引
+      currentSongTime: 0, //当前歌曲播放的时间
+      currentSongTotalTime: 0, //当前播放歌曲的总时长
+      isPlay: true //歌曲的播放状态，true播放，false暂停
+    }
+  }
+  render() {
+    return(
+      <div className="player">
+        <audio></audio>
+        <div className="left-content">
+          <span className="pre-song"></span>
+          <span className="next-song"></span>
+        </div>
+
       </div>
     )
   }
@@ -63,26 +72,23 @@ class Player extends React.Component {
 class MusicPlayer extends React.Component {
   constructor(props){
     super(props);
-    this.constant = {
-
-    };
-    this.state = {
-
+    this.state={
+      //初始化音乐播放器的状态
+      currentSongIndex: 0, //当前播放歌曲的索引
+      currentSongTime: 0, //当前歌曲播放的时间
+      currentSongTotalTime: 0, //当前播放歌曲的总时长
+      isPlay: true //歌曲的播放状态，true播放，false暂停
     }
   }
   render(){
     return(
       <div id="wrapper">
-        <div id="search-bar-wrapper">
-          <SearchBar/>
+
+        <div id="song-list-wrapper">
+          <SongList/>
         </div>
-        <div id="main">
-          <div id="song-list-wrapper">
-            <SongList/>
-          </div>
-          <div id="lyrics-list-wrapper">
-            <LyricsList/>
-          </div>
+        <div id="song-info-wrapper">
+          <SongInfo/>
         </div>
         <div id="player-list">
           <Player/>
@@ -94,7 +100,7 @@ class MusicPlayer extends React.Component {
   }
 }
 
-Player.defaultProps = {
+MusicPlayer.defaultProps = {
 };
 
-export default Player;
+export default MusicPlayer;
