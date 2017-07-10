2d5260666c8ea625f06d3c06f0a2fdcba686d5c6
yangsurui <yangtvxq@163.com>
2017-07-10 23:50:50 +0800

    7.10
    1. 本地文件名不能包含空格，否则路径将无法被识别
    2. 根据json数据包提供的数组对象动态添加歌曲列表中的歌曲及其对应信息

--- src/components/Main.js
+++ src/components/Main.js
@@ -11,22 +11,40 @@ import React from 'react';
  * arr[4][0]: 所属专辑名
  * arr[4][1]: 所属专辑图片路径
  */
-let songInfoArr = require('../data/songData.json');
+let songData = require('../data/songData.json');
+
+class Song extends React.Component {
+  render(){
+    return(
+      <ul className="song-list-content">
+        <li>{this.props.data.number}</li>
+        <li>{this.props.data.songName}</li>
+        <li>{this.props.data.artists}</li>
+        <li>{this.props.data.album.name}</li>
+      </ul>
+    )
+  }
+}
 
 class SongList extends React.Component {
-
   render(){
-    let songList = '';
-    for(let i = 0, j= songInfoArr.length; i<j; i++ ){
-      songList += '<li>'+songInfoArr[i][1]+'</li>';
-    }
+    let songInfo = [];
+    songData.forEach((obj,index)=>{
+      songInfo.push(
+        <Song key={index} data={obj}/>
+      );
+    });
     return(
       <div className="song-list-container">
-        <ul id="song-list">
-          {songList}
+        <ul id="song-list-title">
+          <li>编号</li>
+          <li>歌曲</li>
+          <li>歌手</li>
+          <li>专辑</li>
         </ul>
+        {songInfo}
       </div>
-    )
+    );
   }
 }
 class SongInfo extends React.Component {
@@ -36,36 +54,29 @@ class SongInfo extends React.Component {
     return(
       <div className="song-info-container">
         <div className="album-pic">
-          <img src={}/>
+          <img />
         </div>
         <div className="song-name"></div>
         <div className="singer"></div>
       </div>
-    )
+    );
   }
 }
 class Player extends React.Component {
-  constructor(props){
-    super(props);
-    this.state={
-      //初始化音乐播放器的状态
-      currentSongIndex: 0, //当前播放歌曲的索引
-      currentSongTime: 0, //当前歌曲播放的时间
-      currentSongTotalTime: 0, //当前播放歌曲的总时长
-      isPlay: true //歌曲的播放状态，true播放，false暂停
-    }
-  }
+
   render() {
     return(
       <div className="player">
-        <audio></audio>
+
+        <audio src={this.props.data.url} controls="controls"></audio>
+
         <div className="left-content">
           <span className="pre-song"></span>
           <span className="next-song"></span>
         </div>
 
       </div>
-    )
+    );
   }
 
 }
@@ -80,27 +91,24 @@ class MusicPlayer extends React.Component {
       isPlay: true //歌曲的播放状态，true播放，false暂停
     }
   }
-  render(){
+  render() {
     return(
       <div id="wrapper">
-
         <div id="song-list-wrapper">
           <SongList/>
         </div>
         <div id="song-info-wrapper">
           <SongInfo/>
         </div>
-        <div id="player-list">
-          <Player/>
+        <div id="player-wrapper">
+          <Player data={songData[this.state.currentSongIndex]}/>
         </div>
       </div>
 
-    )
-
+    );
   }
 }
-
 MusicPlayer.defaultProps = {
+  songData : songData
 };
-
 export default MusicPlayer;
